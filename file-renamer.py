f82f87d57d17d8db4791d44fb8bbe434142eaed2
fairfriend92 <fairfriend92@gmail.com>
2026-08-25 14:11:25 +0200

    Added recursive option

--- file-renamer.py
+++ file-renamer.py
@@ -2,20 +2,27 @@ import os
 import unidecode
 from argparse import ArgumentParser
 
+def rename(path, recursive):    
+    for filename in os.listdir(path):
+        name, ext = os.path.splitext(filename)
+        name = name.replace(" ", "-")
+        name = name.replace("_", "-")
+        name_ascii = unidecode.unidecode(name)
+        old_name = path + filename
+        new_name = path + name_ascii.lower() + ext
+        if old_name != new_name:
+            print(old_name + " -> " + new_name)
+            os.rename(old_name, new_name)
+        if recursive and os.path.isdir(new_name):
+            rename(new_name + "/", recursive)            
+
 parser = ArgumentParser(prog="FileRenamer",
                         description="Rename files in a given folder")
 parser.add_argument("-p", "--path",
                     help="Specify path to folder")
+parser.add_argument("-r", "--recursive", action="store_true",
+                    help="Recursively rename subfolders",)
 args = parser.parse_args()
-path = "." if args.path is None else args.path
+root = "." if args.path is None else args.path
 
-for filename in os.listdir(path):
-    name, ext = os.path.splitext(filename)
-    name = name.replace(" ", "-")
-    name = name.replace("_", "-")
-    name_ascii = unidecode.unidecode(name)
-    old_name = path + filename
-    new_name = path + name_ascii.lower() + ext
-    if old_name != new_name:
-        print(old_name + " -> " + new_name)
-        os.rename(old_name, new_name)
+rename(root, args.recursive)
